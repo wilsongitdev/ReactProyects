90657a605e56332d380be0b12afea35bf15b78a1
Wilson <wilson_13_10@hotmail.com>
2024-01-19 17:46:50 -0500

    sad

--- tienda_wilson/src/hooks/useMovies.js
+++ tienda_wilson/src/hooks/useMovies.js
@@ -1,38 +1,38 @@
 import { useCallback, useMemo, useRef, useState } from "react";
 import { searchMovies } from "../services/movies";
 
-export function useMovies({search, sort}) {
+export function useMovies({ search, sort }) {
 
-    const [movies, setMovies] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const previousSearch = useRef(search)
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
-    const getMovies = useCallback(async ({search}) => {// para que no busque la misma pelicula
-        console.log("getMovies")
-        if (search === previousSearch.current) return
+  const getMovies = useCallback(async ({ search }) => {// para que no busque la misma pelicula
+    console.log("getMovies")
+    if (search === previousSearch.current) return
 
-        try {
-          setLoading(true)
-          setError(null)
-          previousSearch.current = search;
-          const newMovies = await searchMovies({search})
-          setMovies(newMovies)
-        } catch (error) {
-          setError(error.message)
-        } finally {
-          setLoading(false)
-        }
-    }, [])
-    
+    try {
+      setLoading(true);
+      setError(null);
+      previousSearch.current = search;
+      const newMovies = await searchMovies({ search });
+      setMovies(newMovies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [])
 
-    const sortedMovies = useMemo(() => {
-        console.log("memo sorted movies")
-        return sort
-          ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
-          : movies
-    }, [sort, movies])
 
-    
-    return { movies: sortedMovies, getMovies, loading, error};
+  const sortedMovies = useMemo(() => {
+    console.log("memo sorted movies")
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+
+  return { movies: sortedMovies, getMovies, loading, error };
 }
